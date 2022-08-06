4281e196972b92303bcf829b768be19d21c7e4b0
dHollingshurst <105985335+dHollingshurst@users.noreply.github.com>
2022-08-06 19:25:46 -0400

    - translating js to jQuery

--- js/script.js
+++ js/script.js
@@ -1,4 +1,37 @@
-let list = $('#test-list');
-list.append("<li>list item</li>");
-let listItem = $('<li></li>');
-list.append(listItem);
+// jQuery
+
+// 1. adding a new item to the list
+let inputValue = $('#input').val();
+let li = $('<li></li>');
+li.append(inputValue);
+
+
+if (inputValue === '') {
+    alert("you must write something!");
+} else {
+    let list = $('#list');
+    list.append(li);
+}
+
+// 2. crossing out an item
+function crossOut() {
+    li.toggleClass("strike");
+}
+
+li.on("dblclick", function crossOut() {
+  li.toggleClass("strike");
+});
+
+// 3 adding the delete button "X"
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+crossOutButton.append(document.createTextNode('X'));
+li.append(crossOutButton);
+
+crossOutButton.on("click", deleteListItem);
+function deleteListItem() {
+    li.addClass("delte");
+}
+// 3.1 adding .delete from css
+
+// 4. reordering the items
+$('#list').sortable();
