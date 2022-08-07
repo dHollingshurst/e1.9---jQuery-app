08e5e0da3962c0a128bc8b1dcbcab9dda4e6f53e
dHollingshurst <105985335+dHollingshurst@users.noreply.github.com>
2022-08-06 20:27:20 -0400

    Update script.js
    creating larger function to envelope script. changing some order of operations

--- js/script.js
+++ js/script.js
@@ -1,48 +1,45 @@
+let toDoList = (function () {
+
 // jQuery
 
 // 1. adding a new item to the list
-
-
+let btn = $('#button');
+let input = $('#input').val()
 
 function newItem() {
-    let inputValue = $('#input').val();
     let li = $('<li></li>');
+    let inputValue = input.val();
     li.append(inputValue);
+
+    if (inputValue === '') {
+        alert("you must write something!");
+    } else {
+        let list = $('#list');
+        list.append(li);
+    }
 }
 
-let btn = $('#button');
-btn.on("click", function newItem() {
-    let inputValue = $('#input').val();
-    let li = $('<li></li>');
-    li.append(inputValue);
-});
 
-if (inputValue === '') {
-    alert("you must write something!");
-} else {
-    let list = $('#list');
-    list.append(li);
-}
+
 
 // 2. crossing out an item. not sure why we can't do this with just li.on event
 function crossOut() {
     li.toggleClass("strike");
 }
 
-li.on("dblclick", function crossOut() {
-  li.toggleClass("strike");
-});
+li.on("dblclick", crossOut);
 
 // 3 adding the delete button "X"
 let crossOutButton = $('<crossOutButton></crossOutButton>');
-crossOutButton.append(document.createTextNode('X'));
+crossOutButton.append("X");
 li.append(crossOutButton);
 
 crossOutButton.on("click", deleteListItem);
 function deleteListItem() {
-    li.addClass("delte");
+    li.addClass("delete");
 }
 // 3.1 adding .delete from css
 
 // 4. reordering the items
 $('#list').sortable();
+});
